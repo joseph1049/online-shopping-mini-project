a1b5606b73f777a1fffad5a4713011d05b2b45d6
Joseph Jerson <josephjersonjj1049@gmail.com>
2024-12-09 14:35:32 +0530

    first commit

---
+++ newslettter.php
@@ -0,0 +1,27 @@
+
+
+        
+
+        <div id="newsletter" class="section" style="background-color: #f8f9fa;">
+			<!-- container -->
+			<div class="container">
+				<!-- row -->
+				<div class="row">
+					<div class="col-md-12">
+					
+						<div class="newsletter">
+							<p>Sign Up for the <strong>OFFERUPDATES</strong></p>
+							<form id="offer_form" onsubmit="return false">
+								<input class="input" type="email" id="email" name="email" placeholder="Enter Your Email">
+								<button class="newsletter-btn" value="Sign Up" name="signup_button" type="submit"><i class="fa fa-envelope"></i> Subscribe</button>
+							</form>
+							<div class="" id="offer_msg">
+                                <!--Alert from signup form-->
+                            </div>
+						</div>
+					</div>
+				</div>
+				<!-- /row -->
+			</div>
+			<!-- /container -->
+		</div>
